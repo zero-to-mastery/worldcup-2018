d6b6aaf6a8f283e56c59c97a1f4749a2b6424377
Thijs Nijhof <tnijhof91@gmail.com>
2018-06-06 15:28:09 +0100

    fixtures showing groupnames and hometeams-awayteams

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -1,10 +1,9 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router';
+import React from 'react';
+// import {withRouter} from 'react-router';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -34,14 +33,14 @@ const styles = {
 class Fixtures extends React.Component {
     state = {
         groups: {}
-    }
+    };
 
+    //Fetch data from json and put it into an object
     componentDidMount() {
         fetch('https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json')
             .then(res => res.json())
             .then(data => {
                 this.setState({groups: data.groups})
-                console.log(this.state)
             })
     }
 
@@ -49,26 +48,25 @@ class Fixtures extends React.Component {
         const {groups} = this.state;
         const {classes} = this.props;
         const groupnames = [];
-        const matches = []
+        // Loop through the groups object from state
+        // Then push the value and the matches into a new array to show the fixtures
+        // We map them below to access their values
         for (let value of Object.values(groups)) {
-            // console.log(value.name)
-            // console.log(value.matches)
-            groupnames.push(value.name);
-            matches.push(value.matches)
-            console.log(matches)
+            groupnames.push({groupname:value.name, matches:value.matches});
         }
-
 
         return (
             groupnames.map((groupname, i) => (
                 <Card key={i} className={classes.card}>
                     <CardContent className={classes.cardContent}>
                         <Typography className={classes.typographyH2} gutterBottom variant="headline" component="h2">
-                            {groupname}
+                            {groupname.groupname}
                         </Typography>
-                        {matches.map((match, i) => (
-                            <p key={i}>{match.name}</p>
-                        ))}
+                        {groupname.matches.map((match, key) => {
+                            return (
+                                <p id={key}>{match.home_team} - {match.away_team}</p>);
+                            })
+                        }
                     </CardContent>
                 </Card>
             ))
