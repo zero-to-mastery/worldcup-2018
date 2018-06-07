037069b1ac1eafcc00846b14b5bf6f7e781aaf33
alex_mac <alexey.rozhin90@gmail.com>
2018-06-07 21:42:09 +0300

    + google map

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppRoutes from './routes';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppRoutes from "./routes";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<AppRoutes />, document.getElementById('root'));
+ReactDOM.render(<AppRoutes />, document.getElementById("root"));
 registerServiceWorker();
