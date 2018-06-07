99d5d843c81bc4cbe46d100d4d717d8492c64802
Kene Udeh <kene.udeh@yahoo.com>
2018-06-07 20:55:14 -0300

    Added date and time to fixture cards

--- src/containers/Fixtures/Fixtures.js
+++ src/containers/Fixtures/Fixtures.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 const styles = {
@@ -26,6 +28,21 @@ const styles = {
   typographyH5: {
     fontSize: ".5em",
     fontWeight: 500
+  }, fixtureCard: {
+    border: "1px solid grey",
+    margin: 5
+  }, team: {
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"flex-start",
+    alignItems:"center"
+  },flag: {
+    height:15,
+    width:30,
+    border: "1px solid black",
+    marginRight: 10
+  }, datetime: {
+    borderLeft: "1px solid grey"
   }
 };
 
@@ -42,10 +59,10 @@ class Fixtures extends React.Component {
     fetch(
       "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json"
     )
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ groups: data.groups, teams: data.teams });
-      });
+    .then(res => res.json())
+    .then(data => {
+      this.setState({ groups: data.groups, teams: data.teams });
+    });
   }
 
   renderFixtures() {
@@ -65,19 +82,48 @@ class Fixtures extends React.Component {
             gutterBottom
             variant="headline"
             component="h2"
-          >
-            {groupname.groupname}
-          </Typography>
-          {groupname.matches.map((match, key) => {
-            let home_team = teams.filter(team => team.id === match.home_team);
-            let away_team = teams.filter(team => team.id === match.away_team);
+            >
+              {groupname.groupname}
+            </Typography>
+            <Grid container spacing={10} className={classes.matchListCard}>
+              {groupname.matches.map((match, key) => {
+                let home_team = teams.filter(team => team.id === match.home_team);
+                let away_team = teams.filter(team => team.id === match.away_team);
+                let matchDate = new Date(match.date);
 
-            return (
-              <p key={key} id={key}>
-                {home_team[0].name} - {away_team[0].name}
-              </p>
-            );
-          })}
+                return (
+                  /*<p key={key} id={key}>
+                  {home_team[0].name} - {away_team[0].name}
+                </p>*/
+                <Grid item xs={12} key={key} className={classes.fixtureCard}>
+                  <Grid container spacing={5}>
+                    <Grid item xs={8} className={classes.fixtureTeams}>
+                      <div className={classes.team}>
+                        <img className={classes.flag} src={home_team[0].flag} alt=""/>
+                        <Typography className={classes.teamName}>
+                          {home_team[0].name}
+                        </Typography>
+                      </div>
+                      <div className={classes.team}>
+                        <img className={classes.flag} src={away_team[0].flag} alt=""/>
+                        <Typography className={classes.teamName}>
+                          {away_team[0].name}
+                        </Typography>
+                      </div>
+                    </Grid>
+                    <Grid item xs={4} className={classes.datetime}>
+                      <Typography>
+                        {matchDate.getDate() + "/" +matchDate.getMonth() + "/" + matchDate.getFullYear()}
+                      </Typography>
+                      <Typography>
+                        {matchDate.getHours() + ":" +matchDate.getMinutes()+"0"}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
         </CardContent>
       </Card>
     ));
