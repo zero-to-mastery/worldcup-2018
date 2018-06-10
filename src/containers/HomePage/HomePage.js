772be7bf2cec72e9a8df7212dcb2214998884c3e
djohal <dilpreet.johal022@gmail.com>
2018-06-10 07:07:53 -0400

    Refactored HomePage

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -9,6 +9,20 @@ class HomePage extends React.Component {
     teams: [],
     knockout: {}
   };
+  
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json"
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          groups: data.groups,
+          teams: data.teams,
+          knockout: data.knockout
+        });
+      });
+  }
 
   /**
    * @param  {object} groups
@@ -71,14 +85,15 @@ class HomePage extends React.Component {
         });
       });
     }
+    
     return _.isEmpty(upcomingMatchGroup) ? null : upcomingMatchGroup;
   }
+
   /**
    * @param  {string} type 'home_team' or 'away_team'
    * @param  {object} matchDetails
    * @returns {object} team details
    */
-
   getTeam(type, matchDetails) {
     let { teams, knockout } = this.state;
     let myTeam;
@@ -98,37 +113,82 @@ class HomePage extends React.Component {
     }
     return myTeam;
   }
-
-  componentDidMount() {
-    fetch(
-      "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json"
-    )
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          groups: data.groups,
-          teams: data.teams,
-          knockout: data.knockout
-        });
-      });
-  }
-  render() {
-    const { groups, knockout } = this.state;
-    let matchDetails = this.getUpcomingMatchDetails(groups);
-    let nextMatchDate = "";
-
+  
+  /**
+   * @param  {object} matchDetails
+   * @returns {Date} next match date
+   */
+  getNextMatchDate(matchDetails) {
     if (!_.isEmpty(matchDetails)) {
-      nextMatchDate = new Date(matchDetails.date).toString();
-    } else {
-      _.values(knockout).map(knockout => {
-        if (knockout.name === "Final") matchDetails = knockout.matches[0];
-        return matchDetails;
-      });
+      return new Date(matchDetails.date).toString();
     }
+  }
 
+  /**
+   * @returns {object} Final Match Details
+   */
+  getFinalMatchDetails() {
+    const { knockout } = this.state;
+    let finalMatch = {};
+
+    _.values(knockout).map(knockout => {
+      if (knockout.name === "Final") {
+        finalMatch = knockout.matches[0];
+      }
+      return null;
+    });
+    return finalMatch    
+  }
+
+  renderTeamDetails() {
+    const { groups } = this.state;
+    let matchDetails = this.getUpcomingMatchDetails(groups);
+    
+    if (_.isEmpty(matchDetails)) {
+      matchDetails = this.getFinalMatchDetails();
+    }
+    
     const home_team = this.getTeam("home_team", matchDetails);
     const away_team = this.getTeam("away_team", matchDetails);
 
+    return (
+      <div className={styles.team_details}>
+        <div className={styles.homepage_flag}>
+          <img
+            className={styles.flag_img_sm}
+            src={home_team[0].flag}
+            alt=""
+          />
+        </div>
+        <div className={styles.title}>
+          <span>{home_team[0].name} </span>
+        </div>
+        <div className={styles.vs}>
+          <span> VS </span>
+        </div>
+        <div className={styles.title}>
+          <span> {away_team[0].name} </span>
+        </div>
+        <div className={styles.homepage_flag}>
+          <img
+            className={styles.flag_img_sm}
+            src={away_team[0].flag}
+            alt=""
+          />
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const { groups } = this.state;
+    let matchDetails = this.getUpcomingMatchDetails(groups);
+    const nextMatchDate = this.getNextMatchDate(matchDetails);
+
+    if (_.isEmpty(matchDetails)) {
+      matchDetails = this.getFinalMatchDetails();
+    }
+    
     return (
       <div>
         {!_.isEmpty(matchDetails) && matchDetails.type !== "winner" ? (
@@ -136,62 +196,14 @@ class HomePage extends React.Component {
             <div className={styles.next_match}>
               <span> Next Match - {nextMatchDate} </span>
             </div>
-            <div className={styles.team_details}>
-              <div className={styles.homepage_flag}>
-                <img
-                  className={styles.flag_img_sm}
-                  src={home_team[0].flag}
-                  alt=""
-                />
-              </div>
-              <div className={styles.title}>
-                <span>{home_team[0].name} </span>
-              </div>
-              <div className={styles.vs}>
-                <span> VS </span>
-              </div>
-              <div className={styles.title}>
-                <span> {away_team[0].name} </span>
-              </div>
-              <div className={styles.homepage_flag}>
-                <img
-                  className={styles.flag_img_sm}
-                  src={away_team[0].flag}
-                  alt=""
-                />
-              </div>
-            </div>
+            {this.renderTeamDetails()}
           </div>
         ) : !_.isEmpty(matchDetails) ? (
           <div className={styles.match_details}>
             <div className={styles.next_match}>
               <span> FIFA World Cup 2018 Winner - {matchDetails.winner} </span>
             </div>
-            <div className={styles.team_details}>
-              <div className={styles.homepage_flag}>
-                <img
-                  className={styles.flag_img_sm}
-                  src={home_team[0].flag}
-                  alt=""
-                />
-              </div>
-              <div className={styles.title}>
-                <span>{home_team[0].name} </span>
-              </div>
-              <div className={styles.vs}>
-                <span> VS </span>
-              </div>
-              <div className={styles.title}>
-                <span> {away_team[0].name} </span>
-              </div>
-              <div className={styles.homepage_flag}>
-                <img
-                  className={styles.flag_img_sm}
-                  src={away_team[0].flag}
-                  alt=""
-                />
-              </div>
-            </div>
+            {this.renderTeamDetails()}
           </div>
         ) : null}
       </div>
