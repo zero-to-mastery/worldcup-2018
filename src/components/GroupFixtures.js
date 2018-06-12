e62842278cf7712cf8c0d9a03e00010040bc6cd9
Kene Udeh <kene.udeh@yahoo.com>
2018-06-12 03:33:59 -0300

    Fixed warnings, GroupFixture cards updates

--- src/components/GroupFixtures.js
+++ src/components/GroupFixtures.js
@@ -2,8 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 const styles = {
